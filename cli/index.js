70ea1bcaf3614008b30c9adb9e3ea212fc206b2e
G Hasini <2410030093@klh.edu.in>
2026-03-04 10:37:47 +0530

    Added start command to CLI

--- cli/index.js
+++ cli/index.js
@@ -1,5 +1,17 @@
 #!/usr/bin/env node
 
-if (process.argv.includes('--help')) {
+const args = process.argv.slice(2);
+const command = args[0];
+
+if (args.includes('--help')) {
   console.log('ShadowAPI CLI');
+  process.exit(0);
+}
+
+switch (command) {
+  case 'start':
+    console.log('Starting ShadowAPI...');
+    break;
+  default:
+    console.log('ShadowAPI CLI');
 }
