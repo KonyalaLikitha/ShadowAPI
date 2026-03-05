f66cfff006ba56208ab590bd1d2f684b56eb8ddc
G Hasini <2410030093@klh.edu.in>
2026-03-05 17:59:01 +0530

    Detect missing configuration file

--- cli/index.js
+++ cli/index.js
@@ -1,7 +1,12 @@
 #!/usr/bin/env node
 
+const fs = require('fs');
+const path = require('path');
+
 const args = process.argv.slice(2);
 const command = args[0];
+
+const configExists = () => fs.existsSync(path.join(process.cwd(), 'shadowapi.config.json'));
 
 if (args.includes('--help')) {
   console.log('ShadowAPI CLI');
@@ -10,6 +15,10 @@ if (args.includes('--help')) {
 
 switch (command) {
   case 'start':
+    if (!configExists()) {
+      console.log('No config found. Run: shadowapi init');
+      process.exit(1);
+    }
     console.log('Starting ShadowAPI...');
     break;
   default:
