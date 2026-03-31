5df9e3fb72d9dedf39f6807f6a92981023a3ae73
Samyukthaa <asksamyukthaa@gmail.com>
2026-03-31 08:59:46 +0530

    fix(gateway): expose x-shadowapi headers in CORS for browser access

--- gateway/middleware.js
+++ gateway/middleware.js
@@ -6,6 +6,7 @@ const corsMiddleware = (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Expose-Headers', 'x-shadowapi-source, x-shadowapi-mode');
   if (req.method === 'OPTIONS') return res.sendStatus(200);
   next();
 };
